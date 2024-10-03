75b6de6cb737c231e12824aa71accd534ae1728f
ignaciojavier27 <nacholin124@gmail.com>
2024-10-03 12:53:16 -0300

    CRUD usuarios

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,9 @@ import cors from 'cors';
 import { config }  from 'dotenv';
 import { sequelize, connectDB } from './config/database.js';
 import { syncModels } from './config/syncModel.js';
+import usuarioRoutes from './routes/usuarioRouter.js';
 
 config();
-
-
-
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +15,10 @@ app.use(json());
 
 connectDB();
 syncModels();
+
+app.use('/api', usuarioRoutes);
+
+
 
 
 app.listen(port, () => {

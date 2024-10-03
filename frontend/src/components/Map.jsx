5955583bb8582189f78e705324d048e7695a23df
ignaciojavier27 <nacholin124@gmail.com>
2024-10-03 19:40:16 -0300

    Funcionalidad Mapa - Mostrar estacionamientos

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,20 +1,52 @@
+import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { obtenerCoordenadas } from '../helpers/obtenerCoordenadas';
 
 const Map = () => {
-  const position = [-34.9854011, -71.2397409];
+  const defaultMapPosition = [-34.9854011, -71.2397409];
+
+  const [estacionamientos, setEstacionamientos] = useState([]);
+
+  // Obtener estacionamientos del backend
+  useEffect(() => {
+    const fetchEstacionamientos = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/estacionamientos');
+        const data = await response.json();
+
+        const estacionamientosConCoordenadas = await Promise.all(
+
+          data.map(async (estacionamiento) => {
+            const coordenadas = await obtenerCoordenadas(estacionamiento.direccion);
+            return { ...estacionamiento, coordenadas };
+          })
+
+        );
+
+        setEstacionamientos(estacionamientosConCoordenadas);
+      } catch (error) {
+        console.error('Error al cargar los estacionamientos:', error);
+      }
+    };
+    fetchEstacionamientos();
+  }, []);
 
   return (
     <section className='container-md'>
-        <MapContainer center={position} zoom={20} style={{ height: '90vh', width: '100%' }}>
+        <MapContainer center={defaultMapPosition} zoom={15} style={{ height: '90vh', width: '100%' }}>
         <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={position}>
-            <Popup>
-            Un marcador en el mapa.
-            </Popup>
-        </Marker>
+        {estacionamientos.map((estacionamiento, index) => (
+          estacionamiento.coordenadas && (
+            <Marker key={index} position={[estacionamiento.coordenadas.latitud, estacionamiento.coordenadas.longitud]}>
+              <Popup>
+                {estacionamiento.nombre} <br /> Dirección: {estacionamiento.direccion}
+              </Popup>
+            </Marker>
+          )
+        ))}
         </MapContainer>
     </section>
   );
